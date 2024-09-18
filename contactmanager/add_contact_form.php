07b56d401abaebd9c252bb46fe29e586ca9fc228
Max Gabriel <Max.Gabriel@triosstudent.com>
2024-09-18 19:47:54 -0400

    added css and add contact form page

---
+++ contactmanager/add_contact_form.php
@@ -0,0 +1,44 @@
+<?php
+require("database.php");// ' or " doesnt really matter in php.
+$queryContacts = 'SELECT * FROM contacts'; //retrieve variable
+$statement1 = $db-> prepare($queryContacts); //connecting data and webpage
+$statement1-> execute(); //running the connection while requested
+$contacts = $statement1-> fetchAll(); //retrieving All the data from the DB with creating the variable
+$statement1-> closeCursor();//after data is fetched the dt connection closes
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Contact manager - Add Contact</title>
+  <link rel="stylesheet" type = "text/css" href="CSS/CSS.css">
+</head>
+<body>
+  <?php include("header.php"); ?>
+  <main>
+    <h2>Add contact</h2>
+    <form action = "add_contact.php" method = "post">
+      <div id ="data">
+        <label for="">First name:</label>
+        <input type="text" name = "first_name"><br>
+        <label for="">Last name:</label>
+        <input type="text" name = "Last_name"><br>
+        <label for="">Email:</label>
+        <input type="text" name = "Email_address"><br>
+        <label for="">Phone:</label>
+        <input type="text" name = "phone"><br>
+      </div>
+      <div id ="buttons">
+        <label for="">Cancel</label>
+        <input type="submit" value = "Save Contact"><br>
+      </div>
+
+    </form>
+    <p><a href="index.php">View contacts</a></p>
+    
+  </main>
+<?php include("footer.php")?>
+</body>
+</html>
--- contactmanager/index.php
+++ contactmanager/index.php
@@ -1,10 +1,10 @@
 <?php
 require("database.php");// ' or " doesnt really matter in php.
-$queryContacts = 'SELECT * FROM contacts';
-$statement1 = $db-> prepare($queryContacts);
-$statement1-> execute();
-$contacts = $statement1-> fetchAll();
-$statement1-> closeCursor();
+$queryContacts = 'SELECT * FROM contacts'; //retrieve variable
+$statement1 = $db-> prepare($queryContacts); //connecting data and webpage
+$statement1-> execute(); //running the connection while requested
+$contacts = $statement1-> fetchAll(); //retrieving All the data from the DB with creating the variable
+$statement1-> closeCursor();//after data is fetched the dt connection closes
 ?>
 
 <!DOCTYPE html>
@@ -13,12 +13,33 @@ $statement1-> closeCursor();
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Contact manager - Home</title>
-  <link rel="stylesheet" type = "text/css" href="CSS.css">
+  <link rel="stylesheet" type = "text/css" href="CSS/CSS.css">
 </head>
 <body>
   <?php include("header.php"); ?>
   <main>
-    <h2>Work in progress</h2>
+    <h2>Contacts</h2>
+    <table>
+      <tr>
+        <th>First Name</th>
+        <th>Last Name</th>
+        <th>Email</th>
+        <th>Phone</th>
+        <th>&nbsp</th> <!-- for edit button -->
+        <th>&nbsp</th> <!-- for delete button -->
+      </tr>
+        <?php foreach($contacts as $contact):?> <!--: instead of { } like in other languages -->
+         <tr>
+          <td><?php echo $contact['firstName'];?></td>
+          <td><?php echo $contact['lastName'];?></td>
+          <td><?php echo $contact['eMail'];?></td>
+          <td><?php echo $contact['phone'];?></td>
+          <td></td> <!-- for edit button -->
+          <td></td> <!-- for delete button -->
+         </tr>
+        <?php endforeach; ?>
+    </table>
+    <p><a href="add_contact_form.php">Add contact</a></p>
     
   </main>
 <?php include("footer.php")?>
